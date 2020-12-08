6c83d1f03e6e1c23e3b9da41d9ba6ce26eb48341
ArielMn22 <arielpaixao10@gmail.com>
2020-12-08 07:54:07 -0300

    Login

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Contatos from './Pages/Contatos';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Contatos" component={Contatos}/>
+      </Tab.Navigator>
+     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ Pages/Contatos/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+const Contatos = () =>{
+    return(
+        <View>
+            <Text>Contatos</Text>
+        </View>
+    )
+}
+
+export default Contatos;
